e0f4b4f70d77d1505d5edfceb5d06998c3c1a96c
Park Won Gyun <51110811+MiewONE@users.noreply.github.com>
2021-10-14 16:20:17 +0900

    subnet 구성 완료

--- 03_VPC/03_VPC2.tf
+++ 03_VPC/03_VPC2.tf
@@ -1,10 +1,10 @@
-resource "aws_vpc" "tf-vpc2" {
-      cidr_block = "192.168.0.0/16"
-      instance_tenancy      = "default"
-      enable_dns_support    = true
-      enable_dns_hostnames  = true
+# resource "aws_vpc" "tf-vpc2" {
+#       cidr_block = "192.168.0.0/16"
+#       instance_tenancy      = "default"
+#       enable_dns_support    = true
+#       enable_dns_hostnames  = true
 
-      tags = {
-        Name = "tf-vpc2"
-  }
-}
+#       tags = {
+#         Name = "tf-vpc2"
+#   }
+# }
