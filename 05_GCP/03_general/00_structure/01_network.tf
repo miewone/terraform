a76ac29a21b357ff895f72cc8b7634183111e3d8
Miewone <dlsrk489@gmail.com>
2021-10-27 22:54:11 +0900

    gcp 2tier 구성

--- 05_GCP/03_general/00_structure/01_network.tf
+++ 05_GCP/03_general/00_structure/01_network.tf
@@ -57,7 +57,8 @@ resource "google_compute_instance" "wgpark-bastioninstance" {
     }
   }
 }
-
+########################################
+#####  오토 스케일링 구성을 위한 부분 #######
 resource "google_compute_instance_template" "wgpark-template-front" {
   name = "${local.name}-instancetemplate"
 
@@ -88,6 +89,7 @@ resource "google_compute_instance_template" "wgpark-template-front" {
   #
 }
 
+
 resource "google_compute_instance_group_manager" "wgpark-instance-group" {
   # require
   # base_insatance_name ,version ,name, zone
@@ -95,9 +97,11 @@ resource "google_compute_instance_group_manager" "wgpark-instance-group" {
 
   base_instance_name = "wgpark-autoscaling"
 
+  target_pools = [google_compute_target_pool.wgpark-tgp.id]
+  target_size  = 2
   # zone을 리전별로할려면 사용할 모든 가용존을 주면됨.
   #  zone = ["${local.region}${var.gcp-az[0]}","${local.region}${var.gcp-az[1]}"]
-  zone = "asia-northeast3-a"
+  zone         = "asia-northeast3-a"
   version {
     instance_template = google_compute_instance_template.wgpark-template-front.id
   }
@@ -116,6 +120,68 @@ resource "google_compute_instance_group_manager" "wgpark-instance-group" {
 
 resource "google_compute_health_check" "wgpark-autohealing" {
   name                = "autohealing-health-check"
+  check_interval_sec  = 5
+  timeout_sec         = 5
+  healthy_threshold   = 2
+  unhealthy_threshold = 10 # 50 seconds
+
+  http_health_check {
+    request_path = "/"
+    port         = "80"
+  }
+}
+
+resource "google_compute_target_pool" "wgpark-tgp" {
+  name = "${local.name}-tgp"
+
+  instances = [
+    "asia-northeast3-a/test1",
+    "asia-northeast3-b/test2",
+  ]
+}
+#####  오토 스케일링 구성을 위한 부분 #######
+########################################
+
+resource "google_compute_global_address" "wgpark-pubip" {
+  name = "${local.name}-pubip"
+}
+
+resource "google_compute_global_forwarding_rule" "wgpark-fr" {
+  name                  = "${local.name}-fr"
+  ip_protocol           = "HTTP"
+  load_balancing_scheme = "EXTERNAL"
+  port_range            = "80"
+  target                = google_compute_target_http_proxy.wgpark-tghttpproxy.id
+  ip_address            = google_compute_global_address.wgpark-pubip.id
+}
+
+resource "google_compute_target_http_proxy" "wgpark-tghttpproxy" {
+  name    = "${local.name}-tg-httpproxy"
+  url_map = google_compute_url_map.wgpark-urlmap.id
+}
+
+resource "google_compute_url_map" "wgpark-urlmap" {
+  name            = "${local.name}-map"
+  default_service = google_compute_backend_service.wgpar-backend.id
+}
+
+resource "google_compute_backend_service" "wgpar-backend" {
+  name                  = "${local.name}-backend-service"
+  protocol              = "HTTP"
+  port_name             = "test"
+  load_balancing_scheme = "EXTERNAL"
+  timeout_sec           = 10
+  enable_cdn            = false
+  health_checks         = [google_compute_health_check.wgpark-httplb-health.id]
+  backend {
+    group = google_compute_instance_group_manager.wgpark-instance-group.instance_group
+    capacity_scaler = 1.0
+  }
+}
+#로드밸런서 상태확인
+resource "google_compute_health_check" "wgpark-httplb-health" {
+
+  name                = "${local.name}-loadhealth"
   check_interval_sec  = 5
   timeout_sec         = 5
   healthy_threshold   = 2
